06798b2b9c709bc18dd2f0602d69e2e822e7a2f2
Evert Kors <evert@hatching.io>
2021-05-21 23:58:51 +0200

    Update solcover config

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,8 @@
 module.exports = {
-  grep: '@skip-on-coverage', // Find everything with this tag
-  invert: true, // Run the grep's inverse set.
+  mocha: {
+    grep: '@skip-on-coverage', // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
   istanbulFolder: './.coverage',
   istanbulReporter: ['html', 'text'],
 };
