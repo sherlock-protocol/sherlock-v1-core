d6c49009f7b59f9114d63dbf4cc04b5c04ea1125
Evert Kors <evert@hatching.io>
2021-05-25 11:30:41 +0200

    Cleanup sherx docstring

--- contracts/interfaces/ISherX.sol
+++ contracts/interfaces/ISherX.sol
@@ -23,8 +23,8 @@ interface ISherX {
   /**
     @notice Sends an event whenever a staker "harvests" earned SHERX
     @notice Harvesting is when SHERX "interest" is staked in the SHERX pool
-    @param user Address of the user for whom SHERX will be harvested
-    @param token Token which has accumulated the SHERX that should be harvested
+    @param user Address of the user for whom SHERX is harvested
+    @param token Token which had accumulated the harvested SHERX
   */
   event Harvest(address indexed user, IERC20 indexed token);
 
@@ -39,14 +39,14 @@ interface ISherX {
   function getTotalUsdPerBlock() external view returns (uint256);
 
   /**
-    @notice Returns the total USD amount of tokens represented by SHERX (underlying SHERX) since last update
-    @return Last stored value of total USD underlying SHERX
+    @notice Returns the internal USD amount of tokens represented by SHERX
+    @return Last stored value of total internal USD underlying SHERX
   */
   function getTotalUsdPoolStored() external view returns (uint256);
 
   /**
-    @notice Returns the calculated total USD amount of tokens represented by SHERX at that block
-    @return Current total USD underlying SHERX
+    @notice Returns the total USD amount of tokens represented by SHERX
+    @return Current total internal USD underlying SHERX
   */
   function getTotalUsdPool() external view returns (uint256);
 
@@ -57,41 +57,40 @@ interface ISherX {
   function getTotalUsdLastSettled() external view returns (uint256);
 
   /**
-    @notice Returns stored USD amount underlying SHERX in the specified token
-    @return Stored USD amount underlying SHERX for _token
+    @notice Returns stored USD amount for `_token`
+    @param _token Token used for protocol premiums
+    @return Stored USD amount
   */
   function getStoredUsd(IERC20 _token) external view returns (uint256);
 
   /**
-    @notice Returns SHERX that has not been minted but should/will be based on underlying collateral
+    @notice Returns SHERX that has not been minted yet
     @return Unminted amount of SHERX tokens
   */
   function getTotalSherXUnminted() external view returns (uint256);
 
   /**
-    @notice Returns total amount of SHERX (minted and unminted)
-    @return total amount of SHERX tokens
+    @notice Returns total amount of SHERX, including unminted
+    @return Total amount of SHERX tokens
   */
   function getTotalSherX() external view returns (uint256);
 
   /**
-    @notice Returns the amount of SHERX to be minted based on USD premiums at the current block
-    @return SHERX created (to be minted later) at the current block
+    @notice Returns the amount of SHERX created per block
+    @return SHERX per block
   */
   function getSherXPerBlock() external view returns (uint256);
 
   /**
-    @notice Returns the amount of SHERX accrued by the address of the sender
-    @return SHERX accrued to msg.sender
-    @question Does this count staked SHERX? Or SHERX accrued to staked SHERX?
+    @notice Returns the total amount of SHERX accrued by the sender
+    @return Total SHERX balance
   */
   function getSherXBalance() external view returns (uint256);
 
   /**
-    @notice Returns the amount of SHERX accrued by the specified address
+    @notice Returns the amount of SHERX accrued by `_user`
     @param _user address to get the SHERX balance of
-    @return SHERX accrued to _user
-    @question Does this count staked SHERX? Or SHERX accrued to staked SHERX?
+    @return Total SHERX balance
   */
   function getSherXBalance(address _user) external view returns (uint256);
 
@@ -109,7 +108,8 @@ interface ISherX {
 
   /**
     @notice Returns the tokens and amounts underlying msg.sender's SHERX balance
-    @return Array of ERC-20 tokens and corresponding arrays of amounts of those tokens
+    @return tokens Array of ERC-20 tokens representing the underlying
+    @return amounts Corresponding amounts of the underlying tokens
   */
   function calcUnderlying()
     external
@@ -117,9 +117,10 @@ interface ISherX {
     returns (IERC20[] memory tokens, uint256[] memory amounts);
 
   /**
-    @notice Returns the tokens and amounts underlying _user's SHERX balance
-    @param _user Address whose underlying SHERX tokens should be queried
-    @return Array of ERC-20 tokens and corresponding arrays of amounts of those tokens
+    @notice Returns the tokens and amounts underlying `_user` SHERX balance
+    @param _user Account whose underlying SHERX tokens should be queried
+    @return tokens Array of ERC-20 tokens representing the underlying
+    @return amounts Corresponding amounts of the underlying tokens
   */
   function calcUnderlying(address _user)
     external
@@ -128,8 +129,9 @@ interface ISherX {
 
   /**
     @notice Returns the tokens and amounts underlying the given amount of SHERX
-    @param _amount Amount of SHERX tokens to find the underlying tokens of
-    @return Array of ERC-20 tokens and corresponding arrays of amounts of those tokens
+    @param _amount Amount of SHERX tokens to calculate the underlying tokens of
+    @return tokens Array of ERC-20 tokens representing the underlying
+    @return amounts Corresponding amounts of the underlying tokens
   */
   function calcUnderlying(uint256 _amount)
     external
@@ -137,15 +139,15 @@ interface ISherX {
     returns (IERC20[] memory tokens, uint256[] memory amounts);
 
   /**
-    @notice Returns the USD amount underlying msg.sender's SHERX (based on stored USD value)
-    @return USD value of SHERX accrued to msg.sender
+    @notice Returns the internal USD amount underlying senders SHERX
+    @return USD value of SHERX accrued to sender
   */
   function calcUnderlyingInStoredUSD() external view returns (uint256);
 
   /**
-    @notice Returns the USD amount underlying the given amount SHERX (based on stored USD value)
+    @notice Returns the internal USD amount underlying the given amount SHERX
     @param _amount Amount of SHERX tokens to find the underlying USD value of
-    @return USD value of the given amount of SHERX
+    @return usd USD value of the given amount of SHERX
   */
   function calcUnderlyingInStoredUSD(uint256 _amount) external view returns (uint256 usd);
 
@@ -154,8 +156,7 @@ interface ISherX {
   //
 
   /**
-    @notice Calculates the SHERX accrued by the specified lockTokens of the sender (from is the sender)
-    @notice When lockTokens are transferred, the accrued SHERX is "harvested" for the sender
+    @notice Function called by lockTokens before transfer
     @param from Address from which lockTokens are being transferred
     @param to Address to which lockTokens are being transferred
     @param amount Amount of lockTokens to be transferred
@@ -167,15 +168,15 @@ interface ISherX {
   ) external;
 
   /**
-    @notice Makes sure all staking pool weights are zero then sets entire weight to the Watsons (security team) initially
+    @notice Set initial SHERX distribution to Watsons
   */
   function setInitialWeight() external;
 
   /**
-    @notice Decides what % of premium payments (SHERX tokens) goes to each token staking pool and to the Watsons (security team)
+    @notice Set SHERX distribution
     @param _tokens Array of tokens to set the weights of
     @param _weights Respective weighting for each token
-    @param _watsons Weighting to set for the Watsons (security team)
+    @param _watsons Weighting to set for the Watsons
   */
   function setWeights(
     IERC20[] memory _tokens,
@@ -184,62 +185,62 @@ interface ISherX {
   ) external;
 
   /**
-    @notice Moves msg.sender's accrued SHERX (accrued from staking ETH, DAI, etc.) into the SHERX staking pool where it can earn interest (in the form of more SHERX)
+    @notice Harvest all tokens on behalf of the sender
   */
   function harvest() external;
 
   /**
-    @notice Moves msg.sender's accrued SHERX from specified token only into the SHERX staking pool where it can earn interest (in the form of more SHERX)
+    @notice Harvest `_token` on behalf of the sender
     @param _token Token to harvest accrued SHERX for
   */
   function harvest(ILock _token) external;
 
   /**
-    @notice Moves msg.sender's accrued SHERX from specified tokens only into the SHERX staking pool where it can earn interest (in the form of more SHERX)
+    @notice Harvest `_tokens` on behalf of the sender
     @param _tokens Array of tokens to harvest accrued SHERX for
   */
   function harvest(ILock[] calldata _tokens) external;
 
   /**
-    @notice Moves accrued SHERX (for specified address) for all tokens into the SHERX staking pool where it can earn interest (in the form of more SHERX)
-    @param _user Address for which to harvest all SHERX
+    @notice Harvest all tokens for `_user`
+    @param _user Account for which to harvest SHERX
   */
   function harvestFor(address _user) external;
 
   /**
-    @notice Moves accrued SHERX (for specified address) for a specified token into the SHERX staking pool where it can earn interest (in the form of more SHERX)
-    @param _user Address for which to harvest
-    @param _token Token for which to harvest
+    @notice Harvest `_token` for `_user`
+    @param _user Account for which to harvest SHERX
+    @param _token Token to harvest
   */
   function harvestFor(address _user, ILock _token) external;
 
   /**
-    @notice Moves accrued SHERX (for specified address) for specified tokens into the SHERX staking pool where it can earn interest (in the form of more SHERX)
-    @param _user Address for which to harvest
-    @param _tokens Array of tokens for which to harvest
+    @notice Harvest `_tokens` for `_user`
+    @param _user Account for which to harvest SHERX
+    @param _tokens Array of tokens to harvest accrued SHERX for
   */
   function harvestFor(address _user, ILock[] calldata _tokens) external;
 
   /**
-    @notice Redeems SHERX tokens for the underlying collateral (ETH, DAI, etc.)
-    @param _amount Amount of SHERX tokens to redeem from msg.sender's SHERX balance
+    @notice Redeems SHERX tokens for the underlying collateral
+    @param _amount Amount of SHERX tokens to redeem
     @param _receiver Address to send redeemed tokens to
   */
   function redeem(uint256 _amount, address _receiver) external;
 
   /**
-    @notice Accrues SHERX to all staking pools and to Watsons based on weightings (and since last accrual)
+    @notice Accrue SHERX based on internal weights
   */
   function accrueSherX() external;
 
   /**
-    @notice Accrues SHERX to specific token's staking pool (since last accrual)
-    @param _token Token representing the staking pool to accrue SHERX to
+    @notice Accrues SHERX to specific token
+    @param _token Token to accure SHERX to.
   */
   function accrueSherX(IERC20 _token) external;
 
   /**
-    @notice Accrues SHERX to the Watsons (security team) based on weighting (and since last accrual)
+    @notice Accrues SHERX to the Watsons.
   */
   function accrueSherXWatsons() external;
 }
