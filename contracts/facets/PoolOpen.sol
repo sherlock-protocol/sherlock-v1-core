be86523e102ca92bef24efebbd9d0f7edcc64b39
Evert Kors <evert@hatching.io>
2021-08-09 18:46:54 +0200

    Remove stupid assembly for storage pointer  #3

--- contracts/facets/PoolOpen.sol
+++ contracts/facets/PoolOpen.sol
@@ -31,27 +31,15 @@ contract PoolOpen is IPoolStake {
   ) internal returns (uint256 lock) {
     require(_amount > 0, 'AMOUNT');
     require(_receiver != address(0), 'RECEIVER');
-    PoolStorage.Base storage ps = baseData();
+    PoolStorage.Base storage ps = baseData(_token);
     require(ps.stakes, 'NO_STAKES');
     _token.safeTransferFrom(msg.sender, address(this), _amount);
 
     lock = LibPool.stake(ps, _amount, _receiver);
   }
 
-  function baseData() internal view returns (PoolStorage.Base storage ps) {
-    ps = PoolStorage.ps(bps());
+  function baseData(IERC20 _token) internal view returns (PoolStorage.Base storage ps) {
+    ps = PoolStorage.ps(_token);
     require(ps.govPool != address(0), 'INVALID_TOKEN');
-  }
-
-  function bps() internal pure returns (IERC20 rt) {
-    // These fields are not accessible from assembly
-    bytes memory array = msg.data;
-    uint256 index = msg.data.length;
-
-    // solhint-disable-next-line no-inline-assembly
-    assembly {
-      // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
-      rt := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
-    }
   }
 }
