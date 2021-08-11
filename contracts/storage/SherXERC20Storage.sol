b5dfc1d6f57cf76180712728c651c9ec374e5c70
Evert Kors <evert@hatching.io>
2021-08-11 11:24:10 +0200

    Lock pragmas #12

--- contracts/storage/SherXERC20Storage.sol
+++ contracts/storage/SherXERC20Storage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.7.1;
+pragma solidity 0.7.6;
 
 /******************************************************************************\
 * Author: Evert Kors <dev@sherlock.xyz> (https://twitter.com/evert0x)
